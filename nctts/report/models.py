d2a99c26bcdec38eba6d16285de517fa0ff96e69
Etkin Getir <94794807+etKing666@users.noreply.github.com>
2022-11-15 22:57:30 +0300

    Models

--- nctts/report/models.py
+++ nctts/report/models.py
@@ -9,11 +9,13 @@ class PublicUser(models.Model):
 
 class Vulnerability(models.Model):
     vulnerability_no = models.AutoField(primary_key=True)
-    reported_by = models.ForeignKey('PublicUser', on_delete=models.CASCADE)
+    reported_by = models.EmailField(max_length=100)
     vulnerable_website = models.URLField(max_length=200)
     date_time = models.DateTimeField(max_length=50)
-    description = models.CharField(max_length=1000)
-    replicate = models.CharField(max_length=1000)
-    exploit_code = models.CharField(max_length=2000)
-    potential_fix = models.CharField(max_length=1000)
+    description = models.TextField(max_length=1000)
+    replicate = models.TextField(max_length=1000)
+    exploit_code = models.TextField(max_length=2000)
+    potential_fix = models.TextField(max_length=1000)
+    video = models.URLField(blank=True, max_length=200)
+
 
--- nctts/report/views.py
+++ nctts/report/views.py
@@ -1,11 +1,29 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . import models
 
 # Create your views here.
 
 def report(request):
-    return render(request, "report.html")
+    if request.method == "POST":
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        email = request.POST['email']
+        vulnerable_website = request.POST['vweb']
+        date_time = request.POST['dtg']
+        description = request.POST['vdesc']
+        replicate = request.POST['vrep']
+        exploit_code = request.POST['expcode']
+        potential_fix = request.POST['pfix']
+        video = request.POST['vlink']
+        ins1 = models.PublicUser(first_name=first_name, last_name=last_name, email=email)
+        ins2 = models.Vulnerability(vulnerable_website=vulnerable_website, date_time=date_time, description=description, replicate=replicate, exploit_code=exploit_code, potential_fix=potential_fix, video=video, reported_by=email)
+        ins1.save()
+        ins2.save()
+        return redirect("/report/success")
+
+    else:
+        return render(request, "report.html")
 
 
 def success(request):
