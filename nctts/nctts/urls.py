7d5674f561163daaf58ee0da22d97363e90cebfc
gicanon <100998384+gicanon@users.noreply.github.com>
2022-11-16 20:03:35 +0100

    update

--- nctts/nctts/urls.py
+++ nctts/nctts/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("homepage/", include('homepage.urls')),
+    path("", include('homepage.urls')),
+    path("lol/", views.LOLView.as_view())
     
 ]
