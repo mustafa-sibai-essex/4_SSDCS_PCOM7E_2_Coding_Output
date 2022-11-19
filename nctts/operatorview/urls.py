d6af9ef2696d99c776377ba0df3b4a93274a92b6
aw22433 <101360136+aw22433@users.noreply.github.com>
2022-11-19 13:13:02 +0000

    Add models and the operatorview

--- nctts/operatorview/urls.py
+++ nctts/operatorview/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ManageRep, name="ManageReports")
+    path("", views.ManageRep, name="ManageReports"),
+    path("<int:vuln_id>", views.vulnerability, name="vulnerability")
 ]
--- nctts/operatorview/views.py
+++ nctts/operatorview/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import VulnerabilitiesOP
 
 # Create your views here.
 
 def ManageRep(request):
-    return render(request, "operatorview/managereport.html")
+    return render(request, "operatorview/managereport.html", {"vulnerabilities": VulnerabilitiesOP.objects.all()})
+
+def vulnerability(request, vuln_id):
+    vulnerability = VulnerabilitiesOP.objects.get(pk=vuln_id)
+    return render(request, "operatorview/vulnerability.html", {"vulnerability": vulnerability})
+
