9d7d7c14efcd24a0b15659443399b0334e694f5e
aw22433 <101360136+aw22433@users.noreply.github.com>
2022-11-20 12:44:13 +0000

    Link to Report models

--- nctts/report/models.py
+++ nctts/report/models.py
@@ -10,8 +10,54 @@ class PublicUser(models.Model):
 
 
 class Vulnerabilities(models.Model):
+    AwaitingApproval = 'Awaiting Approval'
+    Assigned = 'Assigned'
+    Rejected = "Rejected"
+    Fixed = 'Fixed'
+    Escalated = 'Escalated'
+    Ua = 'Unassigned'
+    G_a = 'General Affairs'
+    Agri = "Agriculture, Nature and Food Quality"
+    Def = 'Defence'
+    Econ = 'Economic Affairs'
+    Edu = 'Education'
+    Fin = 'Finance'
+    For_aff = 'Foreign Affairs'
+    Health = 'Health, Welfare and sport'
+    Infra = 'Infrastructure'
+    Int = 'Interior and Kingdom relations'
+    Justice = 'Justice and Security'
+    Soc = 'Social Affairs and Employment'
+    Esc = 'Escalation Team'
+
+    Status_choices = [
+        (AwaitingApproval, 'Awaiting approval'),
+        (Assigned, 'Assigned'),
+        (Rejected, 'Rejected'),
+        (Fixed, 'Fixed'),
+        (Escalated, 'Escalated'),
+    ]
+
+    dept_choices = [
+        (Ua, 'Unassigned'),
+        (G_a, 'General Affairs'),
+        (Agri, "Agriculture, Nature and Food Quality"),
+        (Def, 'Defence'),
+        (Econ, 'Economic Affairs'),
+        (Edu, 'Education'),
+        (Fin, 'Finance'),
+        (For_aff, 'Foreign Affairs'),
+        (Health, 'Health, Welfare and sport'),
+        (Infra, 'Infrastructure'),
+        (Int, 'Interior and Kingdom relations'),
+        (Justice, 'Justice and Security'),
+        (Soc, 'Social Affairs and Employment'),
+        (Esc, 'Escalation Team'),
+    ]
+
     vul_no = models.AutoField(primary_key=True)
-    status = models.TextField(default="Waiting approval")
+    status = models.CharField(max_length=20, choices=Status_choices, default=AwaitingApproval)
+    assigned_to = models.CharField(max_length=100, choices=dept_choices, default=Ua, null=True)
     reported_by = models.ForeignKey(PublicUser, on_delete=models.DO_NOTHING, null=True)
     vulnerable_website = models.URLField(max_length=200)
     date_time = models.DateTimeField(max_length=50)
@@ -20,3 +66,4 @@ class Vulnerabilities(models.Model):
     exploit_code = models.TextField(blank=True, max_length=2000)
     potential_fix = models.TextField(blank=True, max_length=1000)
     video = models.URLField(blank=True, max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
