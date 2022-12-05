a9195dd96ba6ae3233a06984874838f179f4a5b4
O1aDee <94013755+O1aDee@users.noreply.github.com>
2022-12-05 02:18:28 +0000

    Delete nctts directory

--- nctts/operatorview/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from django import forms
-from .models import VulnerabilitiesOP
-
-class CreateNewVuln(ModelForm):
-    class Meta:
-        model = VulnerabilitiesOP
-        fields = '__all__'
-
