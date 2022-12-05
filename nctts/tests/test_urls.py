1f0f51ebd22e0c6c5d499b9ccc26ecee4964181b
O1aDee <94013755+O1aDee@users.noreply.github.com>
2022-12-05 06:17:03 +0000

    updates to tests

---
+++ nctts/tests/test_urls.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from homepage.views import login_user, login_success, login_failed
+from django import setup
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nctts.settings")
+setup()
+
+#Checking to test if homepage url is called and runs with no errors.
+class HomepageURLTest(TestCase):
+    
+    def test_homepage_url(self):
+        url = reverse('login_user')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, login_user)
+    
+    def test_homepage_url(self):
+        url = reverse('login_success')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, login_success)
--- nctts/tests/views_test.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase, Client
-from django.http import HttpRequest
-from django.urls import reverse
-
-
-class TestViews(TestCase):
-    
-    def test_send_mail_GET(self):
-        client = Client()
