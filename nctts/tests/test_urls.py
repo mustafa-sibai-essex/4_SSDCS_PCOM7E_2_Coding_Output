8bf4344e87d3d86ed682c6ee3c57cd680580cb15
O1aDee <94013755+O1aDee@users.noreply.github.com>
2022-12-05 12:44:30 +0000

    update

--- nctts/tests/test_urls.py
+++ nctts/tests/test_urls.py
@@ -7,19 +7,16 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nctts.settings")
 setup()
 
 #Checking to test if homepage url is called and runs with no errors.
-class HomepageURLTest(TestCase):
+class TestsUrls(TestCase):
 
-    def test_login_user(self):
+    def tests_login_user(self):
         url = reverse('login_user')
-        print(resolve(url))
         self.assertEquals(resolve(url).func, login_user)
     
-    def test_login_success(self):
+    def tests_login_success(self):
         url = reverse('login_success')
-        print(resolve(url))
         self.assertEquals(resolve(url).func, login_success)
         
-    def test_login_failed(self):
+    def tests_login_failed(self):
         url = reverse('login_failed')
-        print(resolve(url))
         self.assertEquals(resolve(url).func, login_failed)
