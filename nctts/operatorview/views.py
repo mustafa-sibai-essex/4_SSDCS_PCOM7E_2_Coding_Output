7fc62b065b2bb722efeae3abee77e6d63fecf570
Mustafa Sibai <contact@m-sibai.com>
2022-12-05 11:16:09 +0400

    Thread now is spawned to send an email in the background

--- nctts/operatorview/views.py
+++ nctts/operatorview/views.py
@@ -1,21 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from nctts.sendEmail import SendEmail
 from report.models import *
 from django.contrib.auth.decorators import login_required
 from .forms import CreateNewVuln
 from django.db.models import Q
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
-from .models import models
-from django.core.mail import send_mail
 
 # Create your views here.
+
 
 @login_required
 def ManageRep(request):
     """Renders the Main operator template (managereport)"""
     # also supplies the manage report template with the countAA variable and details of all vulnerabilities from the model
-    countAA= Vulnerabilities.objects.filter(status="Awaiting Approval").count
-    context = {'countAA':countAA, "vulnerabilities": Vulnerabilities.objects.all()}
+    countAA = Vulnerabilities.objects.filter(status="Awaiting Approval").count
+    context = {"countAA": countAA, "vulnerabilities": Vulnerabilities.objects.all()}
     return render(request, "operatorview/managereport.html", context)
 
 
@@ -24,7 +24,9 @@ def vulnerability(request, vuln_id):
     """Render the vulnerabilty template - contains all data on an individual vulnerability"""
     # provides the template with details of a specific vulnerability using the vulnerability ID
     vulnerability = Vulnerabilities.objects.get(pk=vuln_id)
-    return render(request, "operatorview/vulnerability.html", {"vulnerability": vulnerability})
+    return render(
+        request, "operatorview/vulnerability.html", {"vulnerability": vulnerability}
+    )
 
 
 @login_required
@@ -32,59 +34,56 @@ def Add_new(request):
     """Render the Add new vulnerability page - in Operator view"""
     op = request.user
 
-    #uses the currently logged in Operator to auto fill the Reported_by details
-    #requires operator details to exist in the PublicUser model
-    #first it checks if this operator has a profile in the PublicUser table and adds a new profile if required
+    # uses the currently logged in Operator to auto fill the Reported_by details
+    # requires operator details to exist in the PublicUser model
+    # first it checks if this operator has a profile in the PublicUser table and adds a new profile if required
     try:
-        duplicate_check = PublicUser.objects.get(first_name = op)
+        duplicate_check = PublicUser.objects.get(first_name=op)
     except ObjectDoesNotExist:
-        ins_user = PublicUser(first_name= op)
+        ins_user = PublicUser(first_name=op)
         ins_user.save()
     except MultipleObjectsReturned:
-        pass  
-        
-    operator = PublicUser.objects.get(first_name = op)
+        pass
 
-    form=CreateNewVuln()
-    if request.method == 'POST':
-        """ this function will save the completed form as a new vulnerability record in the model."""
+    operator = PublicUser.objects.get(first_name=op)
+
+    form = CreateNewVuln()
+    if request.method == "POST":
+        """this function will save the completed form as a new vulnerability record in the model."""
         form = CreateNewVuln(request.POST)
         if form.is_valid():
             vul = form.save()
-            vul.reported_by= operator         
+            vul.reported_by = operator
             vul.save()
             return redirect("ManageReports")
-    return render(request, "operatorview/Add_new.html", {"form":form})
+    return render(request, "operatorview/Add_new.html", {"form": form})
 
 
 @login_required
 def updatevuln(request, vuln_id):
     """Uses the vuln-id of an existing record to auto-fill the Add new template and allows us to update record"""
     vulnerability = Vulnerabilities.objects.get(pk=vuln_id)
-    form=CreateNewVuln(instance = vulnerability)
-    if request.method == 'POST':
-        form = CreateNewVuln(request.POST, instance = vulnerability)
+    form = CreateNewVuln(instance=vulnerability)
+    if request.method == "POST":
+        form = CreateNewVuln(request.POST, instance=vulnerability)
         if form.is_valid():
             form.save()
-            if vulnerability.status == 'Fixed':
+            if vulnerability.status == "Fixed":
                 if vulnerability.reported_by.email != "":
-                    send_mail(
+                    SendEmail(
                         "NCTTS vulnerability status update",
                         """
 Hello {first_name} {last_name},
 We would like to inform you that your submitted vulnerability status has changed to {status}.""".format(
-                            first_name=vulnerability.reported_by.first_name, 
+                            first_name=vulnerability.reported_by.first_name,
                             last_name=vulnerability.reported_by.last_name,
-                            status=vulnerability.status
+                            status=vulnerability.status,
                         ),
-                        None,
-                        [vulnerability.reported_by.email],
-                        False,
-                    )
-                print(vulnerability.reported_by.email)
-
+                        vulnerability.reported_by.email,
+                    ).start()
             return redirect("ManageReports")
-    return render(request, "operatorview/Add_new.html", {"form":form})
+    return render(request, "operatorview/Add_new.html", {"form": form})
+
 
 @login_required
 def Delete(request, vuln_id):
@@ -95,29 +94,46 @@ def Delete(request, vuln_id):
         return redirect("ManageReports")
     return render(request, "operatorview/delete.html", {"vulnerability": vulnerability})
 
+
 @login_required
 def Awaiting(request):
     """Performs a search of the vulnerabilities awaiting approval and displays them in a template"""
-    return render(request, "operatorview/awaiting.html", {"vulnerabilities": Vulnerabilities.objects.filter(status="Awaiting Approval")})
+    return render(
+        request,
+        "operatorview/awaiting.html",
+        {"vulnerabilities": Vulnerabilities.objects.filter(status="Awaiting Approval")},
+    )
+
 
 @login_required
 def searchresults(request):
     """Performs a search of the vulnerabilities by status or assigned department and displays them in a template"""
     vulnerabilities = []
-    if request.method == 'GET':
-        query = request.GET.get('search')
+    if request.method == "GET":
+        query = request.GET.get("search")
         try:
             vulnerabilities = Vulnerabilities.objects.filter(vul_no=query)
         except:
-            vulnerabilities = Vulnerabilities.objects.filter(Q(status=query) | Q(assigned_to=query))
-    return render(request, "operatorview/searchresults.html", {'query':query, 'vulnerabilities':vulnerabilities})
+            vulnerabilities = Vulnerabilities.objects.filter(
+                Q(status=query) | Q(assigned_to=query)
+            )
+    return render(
+        request,
+        "operatorview/searchresults.html",
+        {"query": query, "vulnerabilities": vulnerabilities},
+    )
+
 
 @login_required
 def searchreference(request):
     """Performs a search of the vulnerabilities by ID and displays them in a template"""
     vulnerabilities = []
-    if request.method == 'GET':
-        query = request.GET.get('search')
+    if request.method == "GET":
+        query = request.GET.get("search")
         if query != "":
             vulnerabilities = Vulnerabilities.objects.filter(vul_no=query)
-    return render(request, "operatorview/searchresults.html", {'query':query, 'vulnerabilities':vulnerabilities})
+    return render(
+        request,
+        "operatorview/searchresults.html",
+        {"query": query, "vulnerabilities": vulnerabilities},
+    )
